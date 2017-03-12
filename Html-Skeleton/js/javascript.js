416dcff8c5785869d28d7f40453c2a155379436d
BenEichner <beneichner@gmail.com>
2017-03-11 21:31:16 -0500

    Add files via upload

---
+++ Html-Skeleton/js/javascript.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+// Click Event
+/*
+	$("selector").click(function(){
+    $(this).css("property","value");
+  });
+*/
+
+
+// Hover Event
+/*
+  $("selector").hover(function(){    
+      $(this).css("property", "value"); // Hover on
+    }, function(){
+      $(this).css("property", "value"); // Hover off
+  });
+*/
+
+
+// Toggle Click
+/*
+  var count = 0;
+
+  $("selector").click(function(){
+    if(count==0)
+    {
+      // Do something
+      count++;
+    }
+    else
+    {
+      // Do something else
+      count--;
+    }
+  });
+*/
+
+});
